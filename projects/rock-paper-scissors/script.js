d357f831128ba937056da77a9843f0adb5280137
Neil McBlane <neil.mcblane@zelim.co>
2025-06-03 11:43:03 +0100

    Implement logic for one round

--- projects/rock-paper-scissors/script.js
+++ projects/rock-paper-scissors/script.js
@@ -6,6 +6,14 @@ const Move = Object.freeze(
     }
 );
 
+const Result = Object.freeze(
+    {
+        USER_WINS: "USER_WINS",
+        DRAW: "DRAW",
+        COMPUTER_WINS: "COMPUTER_WINS",
+    }
+);
+
 let userScore = 0;
 let computerScore = 0;
 
@@ -19,4 +27,39 @@ function getComputerChoice() {
 
 function getUserChoice() {
     return prompt(`Select one of: ${Object.values(Move).join(", ")}`)
+}
+
+function playRound(userChoice, computerChoice) {
+    userChoice = userChoice.toUpperCase();  // Allow users to input in any case
+    const result = getResult(userChoice, computerChoice)
+
+    switch (result) {
+        case Result.USER_WINS:
+            console.log(`You win! ${userChoice} beats ${computerChoice}`);
+            userScore++;
+            break;
+        case Result.DRAW:
+            console.log(`Draw! You both played ${userChoice}`);
+            break;
+        case Result.COMPUTER_WINS:
+            console.log(`You lose! ${computerChoice} beats ${userChoice}`);
+            computerScore++;
+            break;
+    }
+}
+
+function getResult(userChoice, computerChoice) {
+    if (userChoice === computerChoice) return Result.DRAW;
+
+    switch (userChoice) {
+        case Move.ROCK:
+            if (computerChoice === Move.SCISSORS) return Result.USER_WINS;
+            else return Result.COMPUTER_WINS;
+        case Move.PAPER:
+            if (computerChoice === Move.ROCK) return Result.USER_WINS;
+            else return Result.COMPUTER_WINS;
+        case Move.SCISSORS:
+            if (computerChoice === Move.PAPER) return Result.USER_WINS;
+            else return Result.COMPUTER_WINS;
+    }
 }
