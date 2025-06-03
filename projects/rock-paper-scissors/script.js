ea6a5bc63eef5c6d8c02a5fe866a2084fc3813c0
Neil McBlane <neil.mcblane@zelim.co>
2025-06-03 01:30:26 +0100

    Add getComputerChoice function

--- projects/rock-paper-scissors/script.js
+++ projects/rock-paper-scissors/script.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+const Move = Object.freeze(
+    {
+        ROCK: "ROCK",
+        PAPER: "PAPER",
+        SCISSORS: "SCISSORS",
+    }
+);
+
+function getComputerChoice() {
+    randomValue = Math.random();
+
+    if (randomValue < 1/3) return Move.ROCK;
+    if (randomValue < 2/3) return Move.PAPER;
+    return Move.SCISSORS;
+}
