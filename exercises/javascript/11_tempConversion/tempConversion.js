cc0a3b3ce23bdf79223e5686ddc26eee06c6e1d4
NeilMcB <neil.mcblane@zelim.co>
2025-06-04 16:00:10 +0100

    Solve more JavaScript exercises (#18)
    * Solve exercise 06
    
    * Solve exercise 07
    
    * Solve exercise 08
    
    * Solve exercise 09 in a bad way
    
    Just want to do it functionally...
    
    * Solve exercise 10
    
    * Solve exercise 11

--- exercises/javascript/11_tempConversion/tempConversion.js
+++ exercises/javascript/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundToPrecision = function(number, precision) {
+  return Math.round(number * 10 ** precision) / 10 ** precision;
+}
+
+const convertToFahrenheit = function(celsius) {
+  return roundToPrecision(celsius * (9 / 5) + 32, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(farenheit) {
+  return roundToPrecision((farenheit - 32) * (5 / 9), 1);
 };
 
 // Do not edit below this line
