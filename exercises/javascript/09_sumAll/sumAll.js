d75337062e471e0e640b576255d07817af877805
Neil McBlane <neil.mcblane@zelim.co>
2025-06-04 15:09:18 +0100

    Solve exercise 09 in a bad way
    Just want to do it functionally...

--- exercises/javascript/09_sumAll/sumAll.js
+++ exercises/javascript/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    if (!Number.isInteger(start) || !Number.isInteger(stop)) return "ERROR";
+    if ((start < 0) || (stop < 0)) return "ERROR";
 
+    [start, stop] = [start, stop].sort();  // Start should be the lesser of the two numbers
+
+    return Array(stop)
+        .fill(1)
+        .map((x, y) => x + y)
+        .filter(x => x >= start)
+        .reduce((collector, value) => collector + value, 0)
 };
 
 // Do not edit below this line
