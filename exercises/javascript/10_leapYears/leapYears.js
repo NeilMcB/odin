2af863e8a989f749091b3f72dd4968eb18e0f671
Neil McBlane <neil.mcblane@zelim.co>
2025-06-04 15:16:30 +0100

    Solve exercise 10

--- exercises/javascript/10_leapYears/leapYears.js
+++ exercises/javascript/10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const isDivisibleBy = function(number, divisor) {
+    return number % divisor === 0;
+}
 
+const leapYears = function(year) {
+    if (!isDivisibleBy(year, 4)) return false;
+    if (isDivisibleBy(year, 100) && !isDivisibleBy(year, 400)) return false;
+    return true;
 };
 
 // Do not edit below this line
