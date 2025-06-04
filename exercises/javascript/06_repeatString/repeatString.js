5c0afb1a98b8f1561f0701c91dbf705a1aa32d27
Neil McBlane <neil.mcblane@zelim.co>
2025-06-04 14:51:17 +0100

    Solve exercise 06

--- exercises/javascript/06_repeatString/repeatString.js
+++ exercises/javascript/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfRepeats) {
+    if (numberOfRepeats < 0) return "ERROR";
 
+    let repeatedString = ""
+    for (let index = 0; index < numberOfRepeats; index++) {
+        repeatedString += string
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
