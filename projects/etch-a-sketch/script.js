02a3e6c9fbc80ededc59abbdd6133a421c296bc6
Neil McBlane <neil.mcblane@zelim.co>
2025-06-06 18:06:36 +0100

    Allow grid to be reset

--- projects/etch-a-sketch/script.js
+++ projects/etch-a-sketch/script.js
@@ -1,4 +1,5 @@
 const sketchBoard = document.querySelector(".sketch-board");
+const resetButton = document.querySelector("button.reset");
 
 const ROW_ELEMENT_TYPE = "div";
 const CELL_ELEMENT_TYPE = "div";
@@ -6,10 +7,12 @@ const CELL_ELEMENT_TYPE = "div";
 const ROW_CLASS_NAME = "row";
 const CELL_CLASS_NAME = "cell";
 
-const ROW_COUNT = 16;
-const COLUMN_COUNT = 16;
+const DEFAULT_ROW_COUNT = 16;
+const MAX_ROW_COUNT = 100;
+const DEFAULT_COLUMN_COUNT = 16;
+const MAX_COLUMN_COUNT = 100;
 
-resetGrid(ROW_COUNT, COLUMN_COUNT);
+resetGrid(DEFAULT_ROW_COUNT, DEFAULT_COLUMN_COUNT);
 
 function resetGrid(rowCount, columnCount) {
     sketchBoard.replaceChildren();  // remove all existing cells from the grid
@@ -32,3 +35,16 @@ sketchBoard.addEventListener(
     "mouseover",
     e => e.target.classList.add("sketched"),
 );
+
+resetButton.addEventListener(
+    "click",
+    () => {
+        const rowCount = Number.parseInt(prompt(`How many rows? (Max ${MAX_ROW_COUNT})`));
+        const columnCount = Number.parseInt(prompt(`How many columns? (Max ${MAX_COLUMN_COUNT})`));
+
+        resetGrid(
+            Math.min(rowCount, MAX_ROW_COUNT),
+            Math.min(columnCount, MAX_COLUMN_COUNT),
+        );
+    }
+)
