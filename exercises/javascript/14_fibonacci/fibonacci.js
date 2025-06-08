0d1d3b64048b4f6cc9b8ee0cd5cd9efcb44217ef
Neil McBlane <neil.mcblane@zelim.co>
2025-06-09 00:57:08 +0100

    Solve exercise 14

--- exercises/javascript/14_fibonacci/fibonacci.js
+++ exercises/javascript/14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = Number.parseInt(number);
 
+    if (number < 0 || number === NaN) return "OOPS";
+    if (number === 0) return 0;
+    if (number === 1) return 1;
+    return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
