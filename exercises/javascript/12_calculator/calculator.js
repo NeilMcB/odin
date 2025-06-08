aeb55ca0514859f2fb28a363362f5b0fce0401a7
Neil McBlane <neil.mcblane@zelim.co>
2025-06-08 14:03:31 +0100

    Solve exercise 12

--- exercises/javascript/12_calculator/calculator.js
+++ exercises/javascript/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-
+const add = function(left, right) {
+  return left + right;
 };
 
-const subtract = function() {
-
+const subtract = function(left, right) {
+  return left - right;
 };
 
-const sum = function() {
-
+const sum = function(numbers) {
+  return numbers.reduce(
+    (accumulator, next) => accumulator + next,
+    0,
+  );
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce(
+    (accumulator, number) => accumulator * number,
+    1,
+  )
 };
 
-const power = function() {
-
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-
+const factorial = function(number) {
+  if (number) return number * factorial(number - 1);
+  else return 1;
 };
 
 // Do not edit below this line
