83df82d3c343bfd9f0b83b1f6eef9228d1669f6f
Neil McBlane <neil.mcblane@zelim.co>
2025-06-09 19:37:57 +0100

    Implement basic arithmetic operations

---
+++ projects/calculator/script.js
@@ -0,0 +1,15 @@
+const add = function (augend, addend) {
+    return augend + addend;
+};
+
+const subtract = function (minuend, subtrahend) {
+    return minuend - subtrahend;
+};
+
+const multiply = function (muliplier, multiplicand) {
+    return muliplier * multiplicand;
+};
+
+const divide = function (dividend, divisor) {
+    return dividend / divisor;
+};
