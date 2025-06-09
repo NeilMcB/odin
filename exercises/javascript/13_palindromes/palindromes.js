066daeedb6504618d5dc6c98560f2ef3e742e39f
NeilMcB <neil.mcblane@zelim.co>
2025-06-09 16:38:53 +0100

    Solve remaining JavaScript exercises (#22)
    * Solve exercise 12
    
    * Solve exercise 13
    
    * Solve exercise 14
    
    * Solve exercise 15
    
    * Solve exercise 16

--- exercises/javascript/13_palindromes/palindromes.js
+++ exercises/javascript/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const filterAlphaNumeric = function (string) {
+    return string
+        .split("")
+        .filter(c => /^[a-zA-Z0-9]$/.test(c))
+        .join()
+}
 
+const palindromes = function (string) {
+    string = filterAlphaNumeric(string).toLowerCase();
+    for (let index = 0; index < string.length / 2; index++) {
+        const forwardsCursor = index;
+        const backwardsCursor = string.length - index - 1;
+        if (string.charAt(forwardsCursor) !== string.charAt(backwardsCursor)) return false;
+    }
+    return true
 };
 
 // Do not edit below this line
--- exercises/javascript/16_findTheOldest/findTheOldest.js
+++ exercises/javascript/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people
+    .map(
+        p => (
+            {
+                ...p,
+                age: (p.yearOfDeath ?? new Date().getFullYear()) - p.yearOfBirth,
+            }
+        )
+    )
+    .reduce(
+        (oldest, p) => oldest = p.age > oldest.age ? p : oldest,
+        {age: -1},
+    )
 };
 
 // Do not edit below this line
