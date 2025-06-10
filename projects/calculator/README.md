e8eb81ddc380cfd83f7335c4daf892899e545dc1
NeilMcB <neil.mcblane@zelim.co>
2025-06-10 01:33:40 +0100

    Implement Calculator project (#23)
    * Initialise calculator project
    
    * Add project boilerplate
    
    * Implement basic arithmetic operations
    
    * Add variables for holding operands and operaton
    
    * Add operate function
    
    * Add HTML elements
    
    * Style elements
    
    * Make equals and clear buttons contrasting colour
    
    * Update display when buttons are pressed
    
    * Show latest digit pressed on digit click
    
    * Build integers from consecutive button presses
    
    * Display operators when clicked
    
    * Make operator functional iff leftOperand has been defined
    
    * Update leftOperand iff no operator has been selected
    
    * Allow rightOperand to be built once leftOperand and operator are defined
    
    * Abstract display logic out of each click listener
    
    * Abstract display logic into dedicated function
    
    * Abstract operand handling into dedicated function
    
    * Add logic for equals
    
    * Add logic for clear button
    
    * Migrate button logic to helpers
    
    * Allow calculations to be chained
    
    * Handle float results
    
    * Add informative error to decimal
    
    * Add note on unhandled edge cases
    
    * Correct README title

---
+++ projects/calculator/README.md
@@ -0,0 +1,10 @@
+# Calculator
+
+Implemenetation of The Odin Project's [calculator assignment](https://www.theodinproject.com/lessons/foundations-calculator).
+
+Mostly complete, I have not:
+
+1. Implemented any kind of display overflow handling
+1. Implemented any extra credit exercises
+
+These might be informative, but I'd rather invest the time in new concepts.
---
+++ projects/calculator/script.js
@@ -0,0 +1,123 @@
+let leftOperand;
+let rightOperand;
+let operator;
+
+
+const add = function (augend, addend) {
+    return augend + addend;
+};
+
+const subtract = function (minuend, subtrahend) {
+    return minuend - subtrahend;
+};
+
+const multiply = function (muliplier, multiplicand) {
+    return muliplier * multiplicand;
+};
+
+const divide = function (dividend, divisor) {
+    return dividend / divisor;
+};
+
+
+const operate = function (leftOperand, operation, rightOperand) {
+    return operation(leftOperand, rightOperand);
+};
+
+
+
+const container = document.querySelector(".container");
+const display = document.querySelector(".content");
+
+const refreshDisplay = function () {
+    display.textContent = `${leftOperand ?? ""} ${getOperatorSymbol(operator) ?? ""} ${rightOperand ?? ""}`;
+}
+
+document.addEventListener("DOMContentLoaded", refreshDisplay);
+
+container.addEventListener(
+    "click",
+    event => {
+        if (event.target.classList.contains("digit")) handleDigit(event.target);
+        else if (event.target.classList.contains("operator")) handleOperator(event.target);
+        else if (event.target.id  === "equals") handleEquals();
+        else if (event.target.classList.contains("clear")) handleClear();
+        else if (event.target.id === "decimal") alert("PREMIUM FEATURE");
+
+        refreshDisplay();
+    },
+);
+
+const handleDigit = function (digitButton) {
+    // If no operator has been selected then we are constructing the left operand
+    if (operator === undefined) {
+        leftOperand = updateOperand(leftOperand, digitButton);
+    }
+    else {
+        rightOperand = updateOperand(rightOperand, digitButton);
+    }
+}
+
+const handleOperator = function (operatorButton) {
+    if (leftOperand === undefined) {
+        // If no left operand has been defined then we do nothing
+        return;
+    }
+
+    if (rightOperand !== undefined) {
+        // If we have already built the right operand then execute the existing calculation first
+        handleEquals();
+    }
+
+    operator = getOperator(operatorButton);
+}
+
+const handleEquals = function () {
+    if (leftOperand === undefined || operator === undefined || rightOperand === undefined) {
+        // If we don't have all the required components of an operation then do nothing
+        return;
+    }
+
+    // Apply the operation, store the result ready for future operations...
+    leftOperand = operate(leftOperand, operator, rightOperand);
+    // ...and clear the memory ready for future operations.
+    rightOperand = undefined;
+    operator = undefined;
+}
+
+const handleClear = function () {
+    // Clear all global memory
+    leftOperand = undefined;
+    operator = undefined;
+    rightOperand = undefined;
+}
+
+const updateOperand = function (operand, digitButton) {
+    return Number.parseFloat(String(operand ?? "") + digitButton.id);
+}
+
+const getOperatorSymbol = function (operator) {
+    switch (operator) {
+        case add:
+            return "+";
+        case subtract:
+            return "−";
+        case multiply:
+            return "×";
+        case divide:
+            return "÷";
+    }
+};
+
+const getOperator = function (button) {
+    switch (button.id) {
+        case "add":
+            return add;
+        case "subtract":
+            return subtract;
+        case "multiply":
+            return multiply;
+        case "divide":
+            return divide;
+    }
+}
